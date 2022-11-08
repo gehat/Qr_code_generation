0d64ec09d9ee8de49e995711956617b85dc76cfd
Leonid <60075077+gehat@users.noreply.github.com>
2022-11-08 21:02:09 +0300

    Update oblako.py

--- oblako.py
+++ oblako.py
@@ -5,7 +5,7 @@ from qr import qrsearch
 def edit_oblako(name, author, url):
 
 
-    y = yadisk.YaDisk(token="AQAAAAAzOwnEAAf56nUrxnY-s0QdhKan9z7tgzE")
+    y = yadisk.YaDisk(token="")
 
     y.download("/Полка/Database.txt", "file.txt")
     with open("file.txt", "a+", encoding='utf-8') as f:
